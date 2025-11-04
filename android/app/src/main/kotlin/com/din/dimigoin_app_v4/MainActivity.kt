4ac3e5ed9c3880d6865648d7ae8545296d68dd89
vvcnyy <yangwoochan68@gmail.com>
2025-11-05 08:43:20 +0900

    fix: splash api

--- android/app/src/main/kotlin/com/din/dimigoin_app_v4/MainActivity.kt
+++ android/app/src/main/kotlin/com/din/dimigoin_app_v4/MainActivity.kt
@@ -1,16 +1,15 @@
-package com.din.dimigoin_app_v4
-
 import android.os.Build
 import android.os.Bundle
-import androidx.core.view.WindowCompat
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash: SplashScreen = installSplashScreen()
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                splashScreenView.remove()
-            }
+            splash.setOnExitAnimationListener { it.remove() }
         }
 
         super.onCreate(savedInstanceState)
