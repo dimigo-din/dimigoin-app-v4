895ce8dcc00b1c658290fb4b2c2a2547340860b6
vvcnyy <yangwoochan68@gmail.com>
2025-12-08 12:35:04 +0900

    fix: push notification and setting page

---
+++ lib/app/services/push/model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NotificationSubject _$NotificationSubjectFromJson(Map<String, dynamic> json) =>
+    NotificationSubject(
+      id: json['id'] as String,
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$NotificationSubjectToJson(
+  NotificationSubject instance,
+) => <String, dynamic>{'id': instance.id, 'description': instance.description};
