336331b20509eace7ef39ea01d64a8bfbe0875f6
vvcnyy <yangwoochan68@gmail.com>
2026-02-26 10:44:04 +0900

    fix: null check

--- lib/app/services/auth/service.dart
+++ lib/app/services/auth/service.dart
@@ -33,9 +33,7 @@ class AuthService extends GetxController {
     final u = _user.value;
     if (u == null) return false;
 
-    return u!.userGrade! >= 1 &&
-          u!.userClass! >= 1 &&
-          u!.gender!.isNotEmpty;
+    return u!.userGrade != null && u!.userClass != null && u!.gender != null && u.gender!.isNotEmpty;
   }
 
   final Completer<void> _initCompleter = Completer<void>();
