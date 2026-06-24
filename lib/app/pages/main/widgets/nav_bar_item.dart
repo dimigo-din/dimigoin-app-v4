07e4e7329593062da65ee8ea1664b4d12d3e5699
vvcnyy <yangwoochan68@gmail.com>
2026-06-24 20:40:15 +0900

    Revert "fix: bottom nav bar icon"
    This reverts commit c9cf993024045c567e61aa1dae847df43935b29a.

--- lib/app/pages/main/widgets/nav_bar_item.dart
+++ lib/app/pages/main/widgets/nav_bar_item.dart
@@ -31,7 +31,7 @@ class NavBarItem extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               SvgPicture.asset(
-                isSelected ? item.filledIconUrl : item.iconUrl,
+                item.iconUrl,
                 width: 28,
                 height: 28,
                 colorFilter: ColorFilter.mode(
